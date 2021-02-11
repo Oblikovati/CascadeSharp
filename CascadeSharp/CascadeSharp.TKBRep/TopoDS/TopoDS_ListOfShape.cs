852758dcd5d141037da1699c5cfc19c087f90fb2
Vinicius Miguel <vmiguel1@gmail.com>
2021-02-11 22:27:48 +0100

    Implemented TopoDS_ListOfShape

--- CascadeSharp/CascadeSharp.TKBRep/TopoDS/TopoDS_ListOfShape.cs
+++ CascadeSharp/CascadeSharp.TKBRep/TopoDS/TopoDS_ListOfShape.cs
@@ -11,84 +11,23 @@ namespace CascadeSharp.TKBRep.TopoDS
     //---------------------------------------------------------------------
     //  Class  TopoDS_ListOfShape
     //---------------------------------------------------------------------
-    public sealed class TopoDS_ListOfShape
+    public sealed class TopoDS_ListOfShape : NCollection_List<TopoDS_Shape>
     {
         public TopoDS_ListOfShape()
-            : base()
         {
-            throw new NotImplementedException();
-        }
 
-        public TopoDS_ListOfShape(NCollection_BaseAllocator theAllocator)
-            : base()
-        {
-            throw new NotImplementedException();
         }
 
         public TopoDS_ListOfShape(TopoDS_ListOfShape theOther)
-            : base()
+            : base(theOther)
         {
-            throw new NotImplementedException();
+
         }
 
-        public int Size()
-        {
-            throw new NotImplementedException();
-        }
 
         public TopoDS_ListOfShape Assign(TopoDS_ListOfShape theOther)
         {
-            throw new NotImplementedException();
+            return (TopoDS_ListOfShape) base.Assign(theOther);
         }
-
-        public void Clear(NCollection_BaseAllocator theAllocator)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        public TopoDS_Shape First()
-        {
-            throw new NotImplementedException();
-        }
-
-        public TopoDS_Shape Last()
-        {
-            throw new NotImplementedException();
-        }
-
-        public TopoDS_Shape Append(TopoDS_Shape theItem)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Append(TopoDS_ListOfShape theOther)
-        {
-            throw new NotImplementedException();
-        }
-
-        public TopoDS_Shape Prepend(TopoDS_Shape theItem)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Prepend(TopoDS_ListOfShape theOther)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RemoveFirst()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Reverse()
-        {
-            throw new NotImplementedException();
-        }
-    }; // class TopoDS_ListOfShape
+    }
 }
