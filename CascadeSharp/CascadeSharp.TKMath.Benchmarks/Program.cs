6ae71d5859faab0d8013b69522b65e80f9121746
Vinicius Miguel <vmiguel1@gmail.com>
2021-02-11 12:28:06 +0100

    Included benchmark project for TKMath

---
+++ CascadeSharp/CascadeSharp.TKMath.Benchmarks/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+using CascadeSharp.TKMath.Benchmarks.math;
+
+namespace CascadeSharp.TKMath.Benchmarks
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Benchmark Runner for TKMath");
+
+            var summary = BenchmarkRunner.Run<MathBenchmarks>();
+        }
+    }
+}
---
+++ CascadeSharp/CascadeSharp.TKMath.Benchmarks/math/MathBenchmarks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using CascadeSharp.TKMath.math;
+
+namespace CascadeSharp.TKMath.Benchmarks.math
+{
+    public class MathBenchmarks
+    {
+        private Vector gaussPoints;
+        private Vector gaussWeights;
+        private int gaussIndex;
+        public MathBenchmarks()
+        {
+            gaussIndex = new Random().Next(3, TKMath.math.math.GaussPointsMax);
+        }
+
+        [Benchmark]
+        public void GaussPoints() => TKMath.math.math.GaussPoints(gaussIndex, out gaussPoints);
+        [Benchmark]
+        public void GaussWeights() => TKMath.math.math.GaussWeights(gaussIndex, out gaussWeights);
+        [Benchmark]
+        public bool GaussPointsAndWeights() =>
+            TKMath.math.math.OrderedGaussPointsAndWeights(gaussIndex, out gaussPoints, out gaussWeights);
+    }
+}
