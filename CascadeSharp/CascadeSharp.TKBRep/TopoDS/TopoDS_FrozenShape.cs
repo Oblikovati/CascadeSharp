0532f1afc8eac71ccec2e13fe6fb5c961ba44022
Vinicius Miguel <vmiguel1@gmail.com>
2021-02-12 21:59:54 +0100

    implemented frozenshape

--- CascadeSharp/CascadeSharp.TKBRep/TopoDS/TopoDS_FrozenShape.cs
+++ CascadeSharp/CascadeSharp.TKBRep/TopoDS/TopoDS_FrozenShape.cs
@@ -3,7 +3,6 @@
 // General Public License V2
 //---------------------------------------------------------------------
 
-using System;
 using CascadeSharp.TKernel.Standard;
 
 namespace CascadeSharp.TKBRep.TopoDS
@@ -14,51 +13,15 @@ namespace CascadeSharp.TKBRep.TopoDS
     public sealed class TopoDS_FrozenShape : Standard_DomainError
     {
         public TopoDS_FrozenShape()
-            : base()
         {
-            throw new NotImplementedException();
+
         }
 
         public TopoDS_FrozenShape(string theMessage)
-            : base()
+            : base(theMessage)
         {
-            throw new NotImplementedException();
+
         }
 
-        public TopoDS_FrozenShape(TopoDS_FrozenShape parameter1)
-            : base()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Throw()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Raise(string theMessage)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Raise()
-        {
-            throw new NotImplementedException();
-        }
-
-        public TopoDS_FrozenShape NewInstance(string theMessage)
-        {
-            throw new NotImplementedException();
-        }
-
-        public TopoDS_FrozenShape NewInstance()
-        {
-            throw new NotImplementedException();
-        }
-
-        public char get_type_name()
-        {
-            throw new NotImplementedException("Native class returns pointer to integer/double/handle.");
-        }
-    }; // class TopoDS_FrozenShape
+    }
 }
