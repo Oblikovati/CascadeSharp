0ed701a307197899fa6dd7f0de8c6e8919f44738
Vinicius Miguel <vmiguel1@gmail.com>
2021-02-12 08:14:01 +0100

    including list iterator

---
+++ CascadeSharp/CascadeSharp.TKBRep/TopoDS/TopoDS_ListIteratorOfListOfShape.cs
@@ -0,0 +1,18 @@
+﻿using CascadeSharp.TKernel.NCollection;
+
+namespace CascadeSharp.TKBRep.TopoDS
+{
+    public class TopoDS_ListIteratorOfListOfShape : NCollection_List<TopoDS_Shape>.Iterator
+    {
+        public TopoDS_ListIteratorOfListOfShape()
+        {
+            
+        }
+
+        public TopoDS_ListIteratorOfListOfShape(TopoDS_ListOfShape list)
+            : base(list)
+        {
+                    
+        }
+    }
+}
