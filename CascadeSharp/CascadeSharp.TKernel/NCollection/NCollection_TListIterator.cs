62411aa9730096c9ca3d488e887d4581372c3515
Vinicius Miguel <vmiguel1@gmail.com>
2021-02-12 08:13:04 +0100

    including list iterator

---
+++ CascadeSharp/CascadeSharp.TKernel/NCollection/NCollection_TListIterator.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CascadeSharp.TKernel.NCollection
+{
+    public class NCollection_TListIterator<T> : NCollection_BaseList.Iterator
+    {
+        public NCollection_TListIterator()
+        {
+            
+        }
+
+        public NCollection_TListIterator(ref NCollection_BaseList theList)
+            : base(theList)
+        {
+            
+        }
+
+    }
+}
