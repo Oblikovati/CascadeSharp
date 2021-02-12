1461e40caf60b9702c875147426a9b926ff8af69
Vinicius Miguel <vmiguel1@gmail.com>
2021-02-12 10:23:09 +0100

    changed methods to static

--- CascadeSharp/CascadeSharp.TKG3d/TopAbs/TopAbs.cs
+++ CascadeSharp/CascadeSharp.TKG3d/TopAbs/TopAbs.cs
@@ -12,59 +12,48 @@ namespace CascadeSharp.TKG3d.TopAbs
     //---------------------------------------------------------------------
     public sealed class TopAbs
     {
-        public TopAbs()
-            : base()
-        {
-            throw new NotImplementedException();
-        }
-
-        public TopAbs(TopAbs parameter1)
-            : base()
-        {
-            throw new NotImplementedException();
-        }
 
         public static TopAbs_Orientation Compose(TopAbs_Orientation Or1, TopAbs_Orientation Or2)
         {
             throw new NotImplementedException();
         }
 
-        public TopAbs_Orientation Reverse(TopAbs_Orientation Or)
+        public static TopAbs_Orientation Reverse(TopAbs_Orientation Or)
         {
             throw new NotImplementedException();
         }
 
-        public TopAbs_Orientation Complement(TopAbs_Orientation Or)
+        public static TopAbs_Orientation Complement(TopAbs_Orientation Or)
         {
             throw new NotImplementedException();
         }
 
-        public string ShapeTypeToString(TopAbs_ShapeEnum theType)
+        public static string ShapeTypeToString(TopAbs_ShapeEnum theType)
         {
             throw new NotImplementedException();
         }
 
-        public TopAbs_ShapeEnum ShapeTypeFromString(string theTypeString)
+        public static TopAbs_ShapeEnum ShapeTypeFromString(string theTypeString)
         {
             throw new NotImplementedException();
         }
 
-        public bool ShapeTypeFromString(string theTypeString, ref TopAbs_ShapeEnum theType)
+        public static bool ShapeTypeFromString(string theTypeString, ref TopAbs_ShapeEnum theType)
         {
             throw new NotImplementedException();
         }
 
-        public string ShapeOrientationToString(TopAbs_Orientation theOrientation)
+        public static string ShapeOrientationToString(TopAbs_Orientation theOrientation)
         {
             throw new NotImplementedException();
         }
 
-        public TopAbs_Orientation ShapeOrientationFromString(string theOrientationString)
+        public static TopAbs_Orientation ShapeOrientationFromString(string theOrientationString)
         {
             throw new NotImplementedException();
         }
 
-        public bool ShapeOrientationFromString(string theOrientationString, ref TopAbs_Orientation theOrientation)
+        public static bool ShapeOrientationFromString(string theOrientationString, ref TopAbs_Orientation theOrientation)
         {
             throw new NotImplementedException();
         }
