70b6ce42eb60301fb010d4304f8a6486725aaf29
Vinicius Miguel <vmiguel1@gmail.com>
2021-02-12 22:25:15 +0100

    Implemented IStream and write method on OStream

---
+++ CascadeSharp/CascadeSharp.TKernel/Standard/Standard_IStream.cs
@@ -0,0 +1,15 @@
+﻿
+using System.IO;
+
+namespace CascadeSharp.TKernel.Standard
+{
+    public abstract class Standard_IStream : Stream
+    {
+        public int Read(out char[] output, int size)
+        {
+            output = new char[size];
+            using var reader = new StreamReader(this);
+            return reader.Read(output, 0, size);
+        }
+    }
+}
--- CascadeSharp/CascadeSharp.TKernel/Standard/Standard_OStream.cs
+++ CascadeSharp/CascadeSharp.TKernel/Standard/Standard_OStream.cs
@@ -4,5 +4,10 @@ namespace CascadeSharp.TKernel.Standard
 {
     public abstract class Standard_OStream : Stream
     {
+        public void Write(string s)
+        {
+            using var writer = new StreamWriter(this);
+            writer.Write(s);
+        }
     }
 }
