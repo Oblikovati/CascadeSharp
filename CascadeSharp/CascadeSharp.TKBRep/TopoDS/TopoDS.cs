7173c8df906b999f4a8f9b38ceef5d6f62093715
Vinicius Miguel <vmiguel1@gmail.com>
2021-02-12 21:39:02 +0100

    Implemented TopoDS

--- CascadeSharp/CascadeSharp.TKBRep/TopoDS/TopoDS.cs
+++ CascadeSharp/CascadeSharp.TKBRep/TopoDS/TopoDS.cs
@@ -3,65 +3,79 @@
 // General Public License V2
 //---------------------------------------------------------------------
 
-using System;
+using CascadeSharp.TKernel.Standard;
+using CascadeSharp.TKG3d.TopAbs;
 
 namespace CascadeSharp.TKBRep.TopoDS
 {
     //---------------------------------------------------------------------
     //  Class  TopoDS
     //---------------------------------------------------------------------
+    /// <summary>
+    /// Provides methods to cast objects of class TopoDS_Shape to be objects of more specialized
+    /// sub-classes. Types are verified, thus in the example below,
+    /// the first two blocks are correct but the third is rejected by the compiler.
+    /// </summary>
     public sealed class TopoDS
     {
-        public TopoDS()
-            : base()
+        private static bool TopoDS_Mismatch(TopoDS_Shape S, TopAbs_ShapeEnum T)
         {
-            throw new NotImplementedException();
+            return S is { } && S.ShapeType() != T;
+        }
+        public static TopoDS_Vertex Vertex(TopoDS_Shape S)
+        {
+            if (TopoDS_Mismatch(S,TopAbs_ShapeEnum.TopAbs_VERTEX))
+                throw new Standard_TypeMismatch("TopoDS.Vertex");
+            return (TopoDS_Vertex) S;
         }
 
-        public TopoDS(TopoDS parameter1)
-            : base()
+        public static TopoDS_Edge Edge(TopoDS_Shape S)
         {
-            throw new NotImplementedException();
+            if (TopoDS_Mismatch(S, TopAbs_ShapeEnum.TopAbs_EDGE))
+                throw new Standard_TypeMismatch("TopoDS.Edge");
+            return (TopoDS_Edge)S;
         }
 
-        public TopoDS_Vertex Vertex(TopoDS_Shape S)
+        public static TopoDS_Wire Wire(TopoDS_Shape S)
         {
-            throw new NotImplementedException();
+            if (TopoDS_Mismatch(S, TopAbs_ShapeEnum.TopAbs_WIRE))
+                throw new Standard_TypeMismatch("TopoDS.Wire");
+            return (TopoDS_Wire)S;
         }
 
-        public TopoDS_Edge Edge(TopoDS_Shape S)
+        public static TopoDS_Face Face(TopoDS_Shape S)
         {
-            throw new NotImplementedException();
+            if (TopoDS_Mismatch(S, TopAbs_ShapeEnum.TopAbs_FACE))
+                throw new Standard_TypeMismatch("TopoDS.Face");
+            return (TopoDS_Face)S;
         }
 
-        public TopoDS_Wire Wire(TopoDS_Shape S)
+        public static TopoDS_Shell Shell(TopoDS_Shape S)
         {
-            throw new NotImplementedException();
+            if (TopoDS_Mismatch(S, TopAbs_ShapeEnum.TopAbs_SHELL))
+                throw new Standard_TypeMismatch("TopoDS.Shell");
+            return (TopoDS_Shell)S;
         }
 
-        public TopoDS_Face Face(TopoDS_Shape S)
+        public static TopoDS_Solid Solid(TopoDS_Shape S)
         {
-            throw new NotImplementedException();
+            if (TopoDS_Mismatch(S, TopAbs_ShapeEnum.TopAbs_SOLID))
+                throw new Standard_TypeMismatch("TopoDS.Solid");
+            return (TopoDS_Solid)S;
         }
 
-        public TopoDS_Shell Shell(TopoDS_Shape S)
+        public static TopoDS_CompSolid CompSolid(TopoDS_Shape S)
         {
-            throw new NotImplementedException();
+            if (TopoDS_Mismatch(S, TopAbs_ShapeEnum.TopAbs_COMPSOLID))
+                throw new Standard_TypeMismatch("TopoDS.CompSolid");
+            return (TopoDS_CompSolid)S;
         }
 
-        public TopoDS_Solid Solid(TopoDS_Shape S)
+        public static TopoDS_Compound Compound(TopoDS_Shape S)
         {
-            throw new NotImplementedException();
+            if (TopoDS_Mismatch(S, TopAbs_ShapeEnum.TopAbs_COMPOUND))
+                throw new Standard_TypeMismatch("TopoDS.Compound");
+            return (TopoDS_Compound)S;
         }
-
-        public TopoDS_CompSolid CompSolid(TopoDS_Shape S)
-        {
-            throw new NotImplementedException();
-        }
-
-        public TopoDS_Compound Compound(TopoDS_Shape S)
-        {
-            throw new NotImplementedException();
-        }
-    }; // class TopoDS
+    }
 }
